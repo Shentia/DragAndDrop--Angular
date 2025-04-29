ca96cc29f7862895b8948956b1123ed4017a48c0
ahmadreza.shamimi <ahmadreza.shamimi@gmail.com>
2025-04-29 03:15:43 +0000

    Final

--- src/app/sidebar-resize/sidebar-resize.component.ts
+++ src/app/sidebar-resize/sidebar-resize.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CdkDragMove, DragDropModule } from '@angular/cdk/drag-drop';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-resize',
-  imports: [],
+  imports: [DragDropModule],
   templateUrl: './sidebar-resize.component.html',
   styleUrl: './sidebar-resize.component.css'
 })
 export class SidebarResizeComponent {
+
+  protected defaultWidth = 250;
+  protected currentWidth = signal(this.defaultWidth)
+
+
+  protected onDragMoved(event:CdkDragMove){
+    this.currentWidth.set(event.pointerPosition.x)
+    const element = event.source.element.nativeElement;
+    element.style.transform ="none"
+  }
+
+protected reset() {
+  this.currentWidth.set(this.defaultWidth)
+}
 
 }
